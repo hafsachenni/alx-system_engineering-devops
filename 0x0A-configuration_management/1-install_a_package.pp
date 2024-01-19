51b6dac777f7f76bfe2681ff7862b80f08c0e842
hafsachenni <hafsachenni77@gmail.com>
2024-01-19 07:36:33 -0800

    werkzeug

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # insatalling flask using puppet
 
-package {'flask':
+package { 'flask':
   ensure   => '2.1.0',
-  provider => pip3,
+  provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure  => '2.1.1',
+  provider => 'pip3',
 }
