cb4b13d403ad997a6a29e7a8cdf0a556aa46323c
hafsachenni <hafsachenni77@gmail.com>
2024-01-20 10:37:05 -0800

    100 done

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,1 +1,7 @@
-#  setting up my client SSH configuration file 
+#  setting up my client SSH configuration file
+
+
+exec {'config SSH client':
+  command => "echo -e 'IdentityFile ~/.ssh/school\nPasswordAuthentication no\n' >> /etc/ssh/ssh_config",
+  path    => '/bin:/usr/bin',
+}
