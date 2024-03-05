0aec6ba1aadeb28ea1968c366adf9d823684c497
hafsa chenni <hafsachenni77@gmail.com>
2024-03-05 19:49:12 +0100

    statuscode

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-"""function that queries the Reddit API"""
+"""
+contains the number_of_subscribers function
+"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """setting a user agent header as reddit's api recommends"""
-    header = {'User-Agent': 'MyBot/1.0 (by /u/hafsachenni)'}
-    """thats the reddit api endpoint for subreddit info"""
-    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = requests.get(url, headers=header)
-    """checking if response is successful"""
-    if response.status_code == 200:
-        data = response.json()
-
-        """checking if the subscribers key is present"""
-        if 'subscribers' in data['data']:
-            return data['data']['subscribers']
-        else:
-            return 0
-    else:
+    """returns the number of subscriberes for a given subreddit"""
+    if subreddit is None or not isinstance(subreddit, str):
         return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': 'Python/requests:APIproject:\
+    v1.0.0 (by /u/hafsachenni)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
