db6b2e9c4b4f117624a0d55ef793d7b42be4cee0
hafsa chenni <hafsachenni77@gmail.com>
2024-04-09 19:01:41 +0200

    sorted count

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -2,8 +2,8 @@
 """this is a function that queries the reddit api
 parses the title of all hot articles
 and prints a sorted count of given keywords"""
-import requests
 from collections import Counter
+import requests
 
 
 def count_words(subreddit, word_list):
