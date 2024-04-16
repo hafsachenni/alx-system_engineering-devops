d20fc68ad13addc1b787eb3beb2677a61f54f2ca
hafsa chenni <hafsachenni77@gmail.com>
2024-04-16 13:35:32 +0200

    failed http requets

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+#fixing nginx to go from 943 failed http request to 0
+
+exec {'replace':
+    provider => shell,
+    command  => 'sudo sed -i \'s/^ULIMIT=.*/ULIMIT="-n 4096"/g\' /etc/default/nginx',
+    before   => Exec['restart'],
+}
+
+exec {'restart':
+    provider => shell,
+    command  => 'sudo service nginx restart',
+}
