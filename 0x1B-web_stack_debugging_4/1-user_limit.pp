bb8ecb33984788d616f56dd42c8a152b25810151
hafsachenni <hafsachenni77@gmail.com>
2024-04-18 11:23:58 +0000

    too many open files

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,6 @@
 # trying to login with holberton user and open a given file without err msg
 
 exec {'replace-1':
-	provider => shell,
 	command  => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
 	before   => Exec['replace-2'],
 }
